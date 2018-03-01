7e3e0cceda7c3d0c0949d55fe60be5df357fc405
Micmu <34965512+Micmu@users.noreply.github.com>
2018-03-01 17:49:14 +0100

    Preparation for new version.

--- src/main/java/net/micmu/mcmods/micwands/MicWandsMod.java
+++ src/main/java/net/micmu/mcmods/micwands/MicWandsMod.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 public class MicWandsMod {
     public static final String MODID = "micwands";
     public static final String NAME = "Mob Control Wands";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
     public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12,1.13)";
     public static final String ACCEPTED_REMOTE_VERSIONS = "[1.0,1.1)";
 
